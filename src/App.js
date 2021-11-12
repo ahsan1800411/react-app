b414f65439530a656b7b6374c09c64e3cd5e27b9
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-11-13 04:41:55 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import React, { useReducer, createContext } from 'react';
+import React from 'react';
 import './App.css';
-// import FetchingOne from './components/FetchingOne';
-import FetchingTwo from './components/FetchingTwo';
-// import CounterContext from './components/CounterContext';
-// import CounterThree from './components/CounterThree';
-// import CounterOne from './components/CounterOne';
-// import CounterTwo from './components/CounterTwo';
-
-// const initialState = {
-//   count: 0,
-// };
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case 'increment':
-//       return { count: state.count + 1 };
-
-//     case 'decrement':
-//       return { count: state.count - 1 };
-
-//     case 'reset':
-//       return { count: 0 };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export const CountContext = createContext();
+import ParentComponent from './components/ParentComponent';
 
 function App() {
-  // const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      {/* <CountContext.Provider value={{ state, dispatch }}>
-        <CounterContext />
-      </CountContext.Provider> */}
-      {/* <FetchingOne /> */}
-      <FetchingTwo />
+      <ParentComponent />
     </>
   );
 }
